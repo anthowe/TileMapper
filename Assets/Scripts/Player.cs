86c756a22d4ecc63f38f76fbcfddbc49ac89c860
anthowe <ahowe2@att.net>
2020-04-03 19:43:02 -0400

    Start anims in script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+    // Config
+    [SerializeField] float runSpeed = 5.0f;
+
+    // State
+    bool isAlive = true;
+    
+    // Cached component references
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    // Messages then methods
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal"); // value between -1 and +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("running", playerHasHorizontalSpeed);
+
+
+    }
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
