f071b9f9b8db0cb96829a7a06c0d3822b98de5a0
anthowe <ahowe2@att.net>
2020-04-07 19:17:37 -0400

    Add Level Exit

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelExit : MonoBehaviour
+{
+
+    void WhwnSomethingHappens()
+    {
+        StartCoroutine(TheThingThatHappens);
+    }
+    IEnumerator TheThingThatHappens
+    {
+        yield with a delay;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+        
+    }
+}
