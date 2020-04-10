7af36fa28685cba9bde2cb7728dc9c16d14f1c78
Anthony Howe <anthowe33@gmail.com>
2020-04-09 20:21:17 -0400

    Add Score

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+
+
+    private void OnTriggerEnter2D(Collider2D myCollider)
+    {
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+
+}
